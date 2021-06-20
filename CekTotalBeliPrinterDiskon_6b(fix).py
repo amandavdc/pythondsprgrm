645cb5c37392295a1a4076228b385d44ea723315
amandavdc <85843625+amandavdc@users.noreply.github.com>
2021-06-20 14:03:05 +0700

    tugas algoritma

---
+++ CekTotalBeliPrinterDiskon_6b(fix).py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 13 21:27:13 2021
+
+@author: Amanda Veby DC, NIM : 20083000092, Kelas : 2E
+"""
+
+# nilai awal variabel = harga 1 printer
+
+HargaPrinter = 660000
+Minimdiskon = 1500000
+jwbprogram = "y"
+while  jwbprogram=="y" or jwbprogram=="Y":
+    
+# input jumlah beli
+    print ("=====================================================")
+    print ("                                                     ")
+    print ("       Transakasi Pembelian Printer EPSON T20        ")
+    print ("                                                     ")
+    print ("=====================================================")
+    jmlbeli = input("jumlah printer epson T20 yang dibeli = ")
+    
+# proses hitung totalawal
+    totalawal = int(HargaPrinter)*int(jmlbeli)
+
+# hasil sementara
+    print("total awal = Rp " + str (totalawal))
+
+# jika pembelian diata 1.5jt, maka diskon 15%
+
+    if int(totalawal)>1500000:
+        diskon = int(totalawal)*15/100
+    elif int(totalawal)<1500000:
+        diskon = int(totalawal)*0/100
+    print("diskon sebesar = Rp " + str (diskon))
+
+# hasil akhir
+    totalakhir = int(totalawal)-int(diskon)
+    print("Total Akhir = Rp " + str (totalakhir))
+    
+# transaksi kembali?
+    jwbprogram = input("Ulang progam ? y/t = ")
+    if jwbprogram=="t" or jwbprogram=="T":
+     break 
---
+++ ceknilaiMhs_5d.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 13 21:18:49 2021
+
+@author: Amanda Veby DC, NIM : 20083000092, Kelas : 2E
+"""
+
+#cek status huruf mahasiswa saat mendapat nilai
+jwbprogram = "y"
+while  jwbprogram=="y" or jwbprogram=="Y":
+    print ("=========================================")
+    print ("                                         ")
+    print ("           Penilaian Mahasiwa            ")
+    print ("                                         ")
+    print ("=========================================")
+    
+    n=1
+    while int(n)>0 and int(n)<=100: 
+#input umur
+        n = input ("Masukkan nilai = ")
+            
+         #cek status
+        if int(n)>0 and int(n)<=100:
+            if int(n)>=91:
+                sts="A"
+            elif int(n)>=81: sts="B"
+            elif int(n)>=71: sts="C"
+            else:
+                sts="D"
+            print(sts)
+
+#ulangi, jika ingin mengecek kembali
+            jwbprogram = input("Ulang progam ? y/t = ")
+            if jwbprogram=="t" or jwbprogram=="T":
+             break 
+        
+        else:
+            pesan=">>> Masukkan angka nilai 0-100 saja"
+            print(pesan)
+            break
+        
+    
+        
+    
+    
+    
